b855de4ed8b57fdd8bc417fbfe8f2aa72bd3555e
🟧 ord.engine <168103355+ordengine@users.noreply.github.com>
2025-12-18 23:55:05 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Greetings} from "./components/Greetings";
+
+function App() {
+
+  return (
+      <div className={'w-full h-full'}>
+          <Greetings />
+      </div>
+  )
+}
+
+export default App
